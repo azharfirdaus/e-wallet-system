0578156290a1d9c32b0a9de5205a5422a41c7811
Azhar Firdaus <azhar.firdaus2@outlook.com>
2026-07-04 15:18:30 +0700

    add endpoints

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/azharfirdaus/e-wallet-system
 
 go 1.26.1
+
+require github.com/gorilla/mux v1.8.1
---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/wallets/{currency_code}", createWalletHandler).Methods(http.MethodPost)
+	router.HandleFunc("/wallets/{id}/topup", topUpWalletHandler).Methods(http.MethodPost)
+	router.HandleFunc("/wallets/{id}/pay", payWithWalletHandler).Methods(http.MethodPost)
+	router.HandleFunc("/wallets/{id}/transfer", transferWalletHandler).Methods(http.MethodPost)
+	router.HandleFunc("/wallets/{id}/suspend", suspendWalletHandler).Methods(http.MethodPost)
+	router.HandleFunc("/wallets/{id}", getWalletHandler).Methods(http.MethodGet)
+
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func createWalletHandler(w http.ResponseWriter, _ *http.Request) {
+	w.WriteHeader(http.StatusNoContent)
+}
+
+func topUpWalletHandler(w http.ResponseWriter, _ *http.Request) {
+	w.WriteHeader(http.StatusNoContent)
+}
+
+func payWithWalletHandler(w http.ResponseWriter, _ *http.Request) {
+	w.WriteHeader(http.StatusNoContent)
+}
+
+func transferWalletHandler(w http.ResponseWriter, _ *http.Request) {
+	w.WriteHeader(http.StatusNoContent)
+}
+
+func suspendWalletHandler(w http.ResponseWriter, _ *http.Request) {
+	w.WriteHeader(http.StatusNoContent)
+}
+
+func getWalletHandler(w http.ResponseWriter, _ *http.Request) {
+	w.WriteHeader(http.StatusNoContent)
+}
